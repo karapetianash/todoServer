58258a972035bc287515b8cd414aac641d311b03
Ashot Karapetian <AshotArtashesi@yandex.ru>
2024-05-13 01:14:12 +0300

    complete the REST API server

--- handlers.go
+++ handlers.go
@@ -1,13 +1,163 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/karapetianash/todo-cli"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var (
+	ErrNoFound     = errors.New("not found")
+	ErrInvalidData = errors.New("invalid data")
+)
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		replyError(w, r, http.StatusNotFound, "")
 		return
 	}
 
 	content := "There's an API here"
 	replyTextContent(w, r, http.StatusOK, content)
+}
+
+// todoRouter dispatches appropriate replying function form incoming request
+func todoRouter(todoFile string, l sync.Locker) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		list := &todo.List{}
+
+		l.Lock()
+		defer l.Unlock()
+
+		if err := list.Get(todoFile); err != nil {
+			replyError(w, r, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		if r.URL.Path == "" {
+			switch r.Method {
+			case http.MethodGet:
+				getAllHandler(w, r, list)
+			case http.MethodPost:
+				addHandler(w, r, list, todoFile)
+			default:
+				message := "Method not supported"
+				replyError(w, r, http.StatusInternalServerError, message)
+			}
+			return
+		}
+
+		id, err := validateID(r.URL.Path, list)
+		if err != nil {
+			if errors.Is(err, ErrNoFound) {
+				replyError(w, r, http.StatusInternalServerError, ErrNoFound.Error())
+				return
+			}
+
+			replyError(w, r, http.StatusInternalServerError, err.Error())
+		}
+
+		switch r.Method {
+		case http.MethodGet:
+			getOneHandler(w, r, list, id)
+		case http.MethodDelete:
+			deleteHandler(w, r, list, id, todoFile)
+		case http.MethodPatch:
+			pathHandler(w, r, list, id, todoFile)
+		default:
+			message := "Method not supported"
+			replyError(w, r, http.StatusInternalServerError, message)
+		}
+	}
+}
+
+// getAllHandler obtains all to-do items
+func getAllHandler(w http.ResponseWriter, r *http.Request, list *todo.List) {
+	resp := &todoResponse{
+		Results: *list,
+	}
+
+	replyJSONContent(w, r, http.StatusOK, resp)
+}
+
+// getOneHandler replies with a single item
+func getOneHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int) {
+	resp := &todoResponse{
+		Results: (*list)[id-1 : id],
+	}
+
+	replyJSONContent(w, r, http.StatusOK, resp)
+}
+
+// deleteHandler deletes item represented by its id
+func deleteHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int, todoFile string) {
+	list.Delete(id)
+	if err := list.Save(todoFile); err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	replyTextContent(w, r, http.StatusNoContent, "")
+}
+
+// patchHandler completes a specific item
+func pathHandler(w http.ResponseWriter, r *http.Request, list *todo.List, id int, todoFile string) {
+	q := r.URL.Query()
+
+	if _, ok := q["complete"]; !ok {
+		message := "Missing query param 'complete'"
+		replyError(w, r, http.StatusBadRequest, message)
+		return
+	}
+
+	list.Complete(id)
+	if err := list.Save(todoFile); err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	replyTextContent(w, r, http.StatusNoContent, "")
+}
+
+// addHandler adds a new item to the list
+func addHandler(w http.ResponseWriter, r *http.Request, list *todo.List, todoFile string) {
+	item := struct {
+		Task string `json:"task"`
+	}{}
+
+	if err := json.NewDecoder(r.Body); err != nil {
+		message := fmt.Sprintf("Invalid JSON: %s", err)
+		replyError(w, r, http.StatusBadRequest, message)
+		return
+	}
+
+	list.Add(item.Task)
+	if err := list.Save(todoFile); err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	replyTextContent(w, r, http.StatusCreated, "")
+}
+
+// validateID ensures the ID provided by user is valid
+func validateID(path string, list *todo.List) (int, error) {
+	id, err := strconv.Atoi(path)
+	if err != nil {
+		return 0, fmt.Errorf("%w: Invalid ID: %s", ErrInvalidData, err)
+	}
+
+	if id < 1 {
+		return 0, fmt.Errorf("%w: Invalid ID: Less than one", ErrInvalidData)
+	}
+
+	if id > len(*list) {
+		return id, fmt.Errorf("%w: ID %d not found", ErrNoFound, id)
+	}
+
+	return id, nil
 }
--- server.go
+++ server.go
@@ -1,11 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+)
 
 func newMux(todoFile string) http.Handler {
 	m := http.NewServeMux()
+	mu := &sync.Mutex{}
 
 	m.HandleFunc("/", rootHandler)
+
+	t := todoRouter(todoFile, mu)
+
+	m.Handle("/todo", http.StripPrefix("/todo", t))
+	m.Handle("/todo/", http.StripPrefix("/todo/", t)) // same result with or without the trailing slash
 
 	return m
 }
@@ -14,4 +25,23 @@ func replyTextContent(w http.ResponseWriter, r *http.Request, status int, conten
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(status)
 	w.Write([]byte(content))
+}
+
+// replyJSONContent replies to a request using JSON data
+func replyJSONContent(w http.ResponseWriter, r *http.Request, status int, resp *todoResponse) {
+	body, err := json.Marshal(resp)
+	if err != nil {
+		replyError(w, r, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(body)
+}
+
+// replyError logs an error and reply to the request with an appropriate HTTP error
+func replyError(w http.ResponseWriter, r *http.Request, status int, message string) {
+	log.Printf("%s %s: Error: %d %s", r.URL, r.Method, status, message)
+	http.Error(w, http.StatusText(status), status)
 }
